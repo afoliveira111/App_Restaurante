91e16c42c8e367398dc4c0f1e0f5952bee352413
Felipe <felipetermometria@hotmail.com>
2023-11-06 18:33:03 -0300

    criando nova classe

---
+++ app/src/main/java/com/example/app_restaurante/NextActivity.java
@@ -0,0 +1,11 @@
+package com.example.app_restaurante;
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NextActivity : AppCompatActivity(){
+        override fun onCreate(savedInstanceState:Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next) // Defina o layout da próxima atividade
+        }
+        }
