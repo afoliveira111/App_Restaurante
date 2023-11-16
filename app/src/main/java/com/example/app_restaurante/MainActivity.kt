57e09fab4d17d2fdb212d7866dc7e19a780f6cbd
Felipe <felipetermometria@hotmail.com>
2023-11-16 12:00:53 -0300

    Menu update

--- app/src/main/java/com/example/app_restaurante/MainActivity.kt
+++ app/src/main/java/com/example/app_restaurante/MainActivity.kt
@@ -7,8 +7,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -18,34 +16,32 @@ class MainActivity : AppCompatActivity() {
     private val foodItems = mutableListOf<FoodItem>()
     private val cartItems = mutableListOf<FoodItem>()
 
-
     private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    private fun addToCart(foodItem: FoodItem){
-            cartItems.add(foodItem)
-        }
-
-
-
         // Inicialize a lista de produtos
-            recyclerView = findViewById(R.id.listaprodutos)
-            foodItems.add(FoodItem("Hamburguer", 10.99f))
-            foodItems.add(FoodItem("Pizza", 15.99f))
-            foodItems.add(FoodItem("Salada", 8.99f))
-            foodItems.add(FoodItem("Refrigerante", 8.99f))
-            foodItems.add(FoodItem("Salada", 8.99f))
+        recyclerView = findViewById(R.id.listaprodutos)
+        foodItems.add(FoodItem("Hamburguer", 10.99f))
+        foodItems.add(FoodItem("Pizza", 15.99f))
+        foodItems.add(FoodItem("Salada", 8.99f))
+        foodItems.add(FoodItem("Refrigerante", 8.99f))
+        foodItems.add(FoodItem("Salada", 8.99f))
 
         // Configure o RecyclerView e atribua o adaptador
-            val adapter = FoodAdapter(foodItems)
-            recyclerView.layoutManager = LinearLayoutManager(this)
-            recyclerView.adapter = adapter
+        val adapter = FoodAdapter(foodItems) { clickedItem ->
             addToCart(clickedItem)
         }
+        recyclerView.adapter = adapter
 
+    }
+
+    private fun addToCart(foodItem: FoodItem) {
+        cartItems.add(foodItem)
+        // Adicione qualquer lógica adicional que você precise ao adicionar um item ao carrinho
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
@@ -55,13 +51,11 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             R.id.carrinho -> {
-                // Lógica para o item 1 do menu
-                showToast("carrinho selecionado")
-                addToCart(cartItems)
+                // Lógica para o item "Carrinho"
+                showToast("Carrinho selecionado")
+                // Adicione aqui a lógica para adicionar itens ao carrinho
                 return true
             }
-
-            // Adicione mais casos conforme necessário
             else -> return super.onOptionsItemSelected(item)
         }
     }
