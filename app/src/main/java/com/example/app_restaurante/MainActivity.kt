e31fbb1c37ca597d37b385dce0f029b43413d35c
Felipe <felipetermometria@hotmail.com>
2023-11-17 18:42:43 -0300

    Menu update

--- app/src/main/java/com/example/app_restaurante/MainActivity.kt
+++ app/src/main/java/com/example/app_restaurante/MainActivity.kt
@@ -1,83 +1,25 @@
 package com.example.app_restaurante
 
-
-
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-
-
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private val cartItems = mutableListOf<FoodItem>()
-
-    private lateinit var recyclerView: RecyclerView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        val btnMenu = findViewById<Button>(R.id.btnmenu)
+
+        btnMenu.setOnClickListener {
+            val intent = Intent(this, NextActivity::class.java)
+            startActivity(intent)
 
 
-        // Inicialize a lista de produtos
-        val foodItems = mutableListOf<FoodItem>()
-        foodItems.add(FoodItem("Hamburguer", 10.99f))
-        foodItems.add(FoodItem("Pizza", 15.99f))
-        foodItems.add(FoodItem("Salada", 8.99f))
-        foodItems.add(FoodItem("Refrigerante", 8.99f))
-        foodItems.add(FoodItem("Salada", 8.99f))
-
-
-        recyclerView = findViewById(R.id.listaprodutos)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-
-        val adapter = FoodAdapter(foodItems) { clickedItem ->
-            addToCart(clickedItem)
         }
-        recyclerView.adapter = adapter
-    }
-
-
-    private fun addToCart(foodItem: FoodItem) {
-        cartItems.add(foodItem)
-        showToast("Produto adicionado ao carrinho")
 
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_item_back -> {
-                // Lógica para voltar para NextActivity
-                val intent = Intent(this, NextActivity::class.java)
-                startActivity(intent)
-                return true
-            }
-
-            R.id.carrinho -> {
-                // Lógica para adicionar itens ao carrinho
-                showToast("Produto adicionado ao carrinho")
-                return true
-            }
-
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
-
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
 }
