b65bd41fb50ab4b5d6a97a544aa352f4c4296149
Felipe <felipetermometria@hotmail.com>
2023-12-01 16:16:08 -0300

    CarrinhoActivity -  button

---
+++ app/src/main/java/com/example/app_restaurante/CarrinhoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app_restaurante
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CarrinhoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_carrinho)
+    }
+}
