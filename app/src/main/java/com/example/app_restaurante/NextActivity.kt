4bb4fb17a698fd040b43d6b0974d668b28ebf77b
Felipe <felipetermometria@hotmail.com>
2023-12-01 13:15:06 -0300

    cartItems

--- app/src/main/java/com/example/app_restaurante/NextActivity.kt
+++ app/src/main/java/com/example/app_restaurante/NextActivity.kt
@@ -3,31 +3,29 @@ package com.example.app_restaurante
 import androidx.appcompat.widget.Toolbar
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class NextActivity : AppCompatActivity() {
 
-    private val cartItems = mutableListOf<FoodItem>()
+    private val cartItems = mutableListOf<FoodItem>() // Mantenha uma lista dos itens no carrinho
 
-    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
 
+        // Configurar a barra de navegação
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-
-
-        // Inicialize a lista de produtos
-        val foodItems = mutableListOf<FoodItem>()
+        // Configurar a lista de produtos
+        val foodItems =
+            mutableListOf<FoodItem>() // Certifique-se de inicializar corretamente a lista
         foodItems.add(FoodItem("Hamburguer", 10.99f))
         foodItems.add(FoodItem("Pizza", 15.99f))
         foodItems.add(FoodItem("Salada", 8.99f))
@@ -35,49 +33,61 @@ class NextActivity : AppCompatActivity() {
         foodItems.add(FoodItem("Salada", 8.99f))
 
 
-        recyclerView = findViewById(R.id.listaprodutos)
+        val recyclerView: RecyclerView = findViewById(R.id.listaprodutos)
         recyclerView.layoutManager = LinearLayoutManager(this)
-
 
         val adapter = FoodAdapter(foodItems) { clickedItem ->
             addToCart(clickedItem)
         }
         recyclerView.adapter = adapter
-    }
 
+        // Configurar o BottomNavigationView
+        val btnInicio: BottomNavigationView = findViewById(R.id.btninício)
+        btnInicio.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_item_back -> {
+                    // Lógica para voltar para MainActivity
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()  // Certifique-se de chamar finish() para encerrar a NextActivity
+                    true
+                }
 
-    private fun addToCart(foodItem: FoodItem) {
-        cartItems.add(foodItem)
-        showToast("Produto adicionado ao carrinho")
+                R.id.carrinho -> {
+                    // Lógica para exibir os itens no carrinho
+                    showCartItems()
+                    true
+                }
 
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_item_back -> {
-                // Lógica para voltar para MainActivity
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                return true
+                else -> false
             }
-
-            R.id.carrinho -> {
-                // Lógica para adicionar itens ao carrinho
-                showToast("Produto adicionado ao carrinho")
-                return true
-            }
-
-            else -> return super.onOptionsItemSelected(item)
         }
+
+        // Outras configurações necessárias
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
-
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    private fun showCartItems() {
+        val cartItemsString = cartItems . joinToString (separator = "\n") { it.name }
+        showToast("Itens no Carrinho:\n$cartItemsString")
     }
 }
+    private fun addToCart(foodItem: FoodItem) {
+    cartItems.add(foodItem)
+    showToast("Item adicionado ao carrinho")
+}
+
+    private fun showCartItems() {
+    // Lógica para exibir os itens no carrinho
+    val cartItemsString = cartItems.joinToString(separator = "\n") { it.name }
+    showToast("Itens no Carrinho:\n$cartItemsString")
+}
+
+    private fun showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+
+
+
+
+
